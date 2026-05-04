89c0e3b9cc022fee27b4cd334edae2d1e10f1847
Meet Vora <125785245+MeetVora79@users.noreply.github.com>
2026-05-04 16:32:21 +0530

    Update API base URLs to production environment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'https://blogify-gcdw.onrender.com/api/blogs',
+	baseURL: 'https://blogify-production-a54b.up.railway.app/api/blogs',
 })
 
 export const authApi = axios.create({
-	baseURL: 'https://blogify-gcdw.onrender.com/api/auth'
+	baseURL: 'https://blogify-production-a54b.up.railway.app/api/auth'
 })
